1e9959becd695795aade5c86e27529f5bd3a6cfa
Unknown <jrohweller13@apu.edu>
2019-01-21 15:21:07 -0800

    Lots of improvements/learning

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import "./SeasonDisplay.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Used with first project.
+// function renderCard() {
+//   return (
+//     <ApprovalCard>
+//       <CommentDetail
+//         avatar={faker.image.avatar()}
+//         author={faker.name.firstName()}
+//         date={"a few seconds ago."}
+//         content={faker.lorem.sentence()}
+//       />
+//     </ApprovalCard>
+//   );
+// }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  renderErrorMessage = () => {};
+
+  render() {
+    const icon = "snowflake";
+    // if (this.state.errorMessage && )
+    return (
+      // <div className="season-display winter ui containter comments ">
+      //   Latitude:{this.state.lat}
+      //   <i className={`icon-left massive ${icon} icon`} />
+      //   <br />
+      //   Error:{this.state.errorMessage}
+      // </div>
+        <Spinner />
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
