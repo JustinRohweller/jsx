85284e6fa110c05dfdae230bf6b2aa63bdb30131
Unknown <jrohweller13@apu.edu>
2019-01-21 17:26:38 -0800

    Prepared for css changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 import unsplash from "../api/unsplash";
+import ImageList from './ImageList';
 
 class App extends React.Component {
+  state = { images: [] }
+
   onSubmit = async term => {
     const response = await unsplash.get(
       "https://api.unsplash.com/search/photos",
@@ -13,12 +16,14 @@ class App extends React.Component {
         }
       }
     );
+    this.setState({ images: response.data.results })
   };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
